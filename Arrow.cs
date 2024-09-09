4dc03d318bb7a59b6937fe063b5c1a2f1c3a707f
johnfn <johnfn@gmail.com>
2024-09-08 20:37:58 -0700

    tint

--- Arrow.cs
+++ Arrow.cs
@@ -52,6 +52,13 @@ public partial class Arrow : Node2D
         }
     }
 
+    [Export]
+    public Color Tint
+    {
+        get => Modulate;
+        set => Modulate = value;
+    }
+
     public static Arrow Instantiate(ArrowType type)
     {
         var arrow = GD.Load<PackedScene>("res://arrow.tscn").Instantiate<Arrow>();
